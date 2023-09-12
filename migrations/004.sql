5b524f63f2515bb26940b6cb784ee14bd792a4d0
Bentley Hensel <bentleyhensel@gmail.com>
2023-09-12 08:32:05 -0400

    Sitemaps Table

---
+++ migrations/004.sql
@@ -0,0 +1,26 @@
+-- Migration: 00
+-- ------------------------------
+-- Sitemaps
+-- ------------------------------
+
+
+CREATE TABLE targets.sitemaps (
+  id bigserial NOT NULL,
+  created_at timestamptz NOT NULL DEFAULT now(),
+  updated_at timestamptz NOT NULL DEFAULT now(),
+  sitemap_url text NOT NULL,
+  last_modified timestamptz NULL,
+  change_frequency text NULL,
+  priority float4 NULL,
+  domain_id int4 NOT NULL,
+  CONSTRAINT sitemaps_pk PRIMARY KEY (id),
+  CONSTRAINT sitemaps_fk FOREIGN KEY (domain_id) REFERENCES targets.domains(id)
+);
+CREATE INDEX idx_sitemaps_domain_id ON targets.sitemaps USING btree (domain_id);
+
+-- Table Triggers
+
+create trigger sitemaps_updated_at_trigger after
+update
+    on
+    targets.sitemaps for each row execute function toolbox.update_timestamp_on_change();
